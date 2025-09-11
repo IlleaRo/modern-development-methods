3f07b68daa31207f7961618065e2e5856f6e73d7
IlleaRo <aro.kharin@gmail.com>
2025-09-11 21:17:26 +0700

    1.2 update

--- src/modern_development_methods/task_1_2.clj
+++ src/modern_development_methods/task_1_2.clj
@@ -1,44 +1,24 @@
 (ns modern-development-methods.task-1-2)
 
-;(defn gen-words
-;  [alphabet n]
-;  (letfn [(go [prev k prefix]
-;            (if (zero? k)
-;              (println prefix)
-;              (loop [cur alphabet]
-;                (when (seq cur)
-;                  (let [c (first cur)]
-;                    (when (not= c prev)
-;                      (go c (dec k) (str prefix c)))
-;                    (recur (rest cur)))))))]
-;    (go nil n "")))
-;
-;
-;
-;
-;(defn -main
-;  [& args]
-;  (if (not= (count args) 2)
-;    (do
-;      (println "Usage: <alphabet> <n>")
-;      (System/exit 1))
-;    (let [alphabet (seq (first args))
-;          n (Integer/parseInt (second args))]
-;      (println "Alphabet:" alphabet " num:" n)
-;      (gen-words alphabet n))
-;    ))
-
 (defn gen-words
-  [alphabet cur_set prev k prefix]
-  (if (zero? k)
-    do (
-         (println prefix)
-         (gen-words alphabet (rest alphabet) nil (count alphabet) ""))
-    (when (seq cur_set)
-      (let [cur (first cur_set)]
-        (when (not= cur prev)
-          (gen-words alphabet (rest cur_set) cur_set (dec k) (str prefix cur)))
-        ))))
+  [alphabet n]
+  (loop [stack (list [nil n ""])]
+    (when-let [[prev k prefix] (first stack)]
+      (if (zero? k)
+        (do
+          (println prefix)
+          (recur (rest stack)))
+        (let [next-stack
+              (loop [cs (seq (reverse alphabet))
+                     s (rest stack)]
+                (if (seq cs)
+                  (let [c (first cs)]
+                    (recur (rest cs)
+                           (if (= c prev)
+                             s
+                             (cons [c (dec k) (str prefix c)] s))))
+                  s))]
+          (recur next-stack))))))
 
 
 (defn -main
@@ -50,5 +30,5 @@
     (let [alphabet (seq (first args))
           n (Integer/parseInt (second args))]
       (println "Alphabet:" alphabet " num:" n)
-      (gen-words alphabet alphabet nil n ""))
+      (gen-words alphabet n))
     ))
