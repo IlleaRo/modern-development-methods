a8ed3376ff5388b3447f8ea47726e37362076efc
IlleaRo <aro.kharin@gmail.com>
2025-09-20 17:35:02 +0700

    Add 1.4

---
+++ src/modern_development_methods/task_1_4.clj
@@ -0,0 +1,33 @@
+(ns modern-development-methods.task-1-4)
+
+(defn gen-words
+  [alphabet n]
+  (map (fn [word] (apply str word))
+       (reduce (fn [word_set _]                                   ; Мы не используем числа 0, 1, 2, ..., n
+                 (reduce (fn [acc word]                               ; расширяем каждое слово word
+                           (let [choices (if (empty? word)
+                                           alphabet
+                                           (filter (fn [x] (not= x (peek word))) alphabet))] ; запрет одинаковых подряд
+                             (reduce (fn [acc ch] (conj acc (conj word ch))) acc choices)))
+                         [] word_set))
+               [[]] (range n)))) ; Нужно сделать n шагов
+
+(defn remove-duplicates
+  [alphabet]
+  (reduce
+   (fn [acc x]
+     (if (not (some #{x} acc))
+       (concat acc (list x))
+       acc))
+   `() alphabet))
+
+(defn -main
+  [& args]
+  (if (not= (count args) 2)
+    (do
+      (println "Usage: <alphabet> <n>")
+      (System/exit 1))
+    (let [alphabet (remove-duplicates (seq (first args)))
+          n (Integer/parseInt (second args))]
+      (println "Alphabet:" alphabet " num:" n)
+      (println (gen-words alphabet n)))))
