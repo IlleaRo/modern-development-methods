c9ca54ad2a8c81bfda197e329c901a2796fab9b8
IlleaRo <aro.kharin@gmail.com>
2025-09-27 20:56:16 +0700

    2.1: Тесты и мемоизация

--- src/modern_development_methods/task_2/task_2_1.clj
+++ src/modern_development_methods/task_2/task_2_1.clj
@@ -1,12 +1,67 @@
 (ns modern-development-methods.task-2.task-2-1)
 
-(def N 100)
-
-(defn get-trapezoid-integral [f x]
-  (let [h (/ x N),
+(defn get-trapezoid-integral [f x h]
+  (let [N (long (Math/floor (/ x h))),
         A (* (f 0) 0.5)
         B (* (f x) 0.5)
         S (+ A
              (reduce + (map (fn [i] (f (* i h))) (range 1 N)))
              B)]
     (* h S)))
+
+(def get-trapezoid-integral-mem-var-1        ; Мемоизация значений первообразной
+  (memoize get-trapezoid-integral))          ; не снизит сложность построения графика
+
+
+(defn summator [f h N]
+  (reduce + (map (fn [i] (f (* i h))) (range 1 N))))
+(def summator-mem ; кэш на узлах i*h
+    (memoize summator))
+
+(defn get-trapezoid-integral-mem-var-2 [f x h]
+  (let [N (long (Math/floor (/ x h))),
+        A (* (f 0) 0.5)
+        B (* (f x) 0.5)
+        S (+ A
+             (summator-mem f h N)
+             B)]
+    (* h S)))
+
+
+(defn bench [label fns]
+  (println "=== " label " ===")
+  (doseq [[tag f] fns]
+    (print tag "-> ")
+    (time (f)))
+  (println))
+
+(defn -main [& _]
+  (let [f (fn [x] (* x x))
+        h 0.1]
+  (bench "Plain trapezoid"
+         [["x=50" #(get-trapezoid-integral f 50 h)]
+          ["x=50" #(get-trapezoid-integral f 50 h)]
+          ["x=48" #(get-trapezoid-integral f 48 h)]
+          ["x=30" #(get-trapezoid-integral f 30 h)]
+          ["x=72" #(get-trapezoid-integral f 72 h)]
+          ["x=71" #(get-trapezoid-integral f 71 h)]
+          ["x=5"  #(get-trapezoid-integral f 5 h)]])
+
+  (bench "Mem var-1"
+         [["x=50" #(get-trapezoid-integral-mem-var-1 f 50 h)]
+          ["x=50" #(get-trapezoid-integral-mem-var-1 f 50 h)]
+          ["x=48" #(get-trapezoid-integral-mem-var-1 f 48 h)]
+          ["x=30" #(get-trapezoid-integral-mem-var-1 f 30 h)]
+          ["x=72" #(get-trapezoid-integral-mem-var-1 f 72 h)]
+          ["x=71" #(get-trapezoid-integral-mem-var-1 f 71 h)]
+          ["x=5"  #(get-trapezoid-integral-mem-var-1 f 5 h)]])
+
+  (bench "Mem var-2"
+         [["x=50" #(get-trapezoid-integral-mem-var-2 f 50 h)]
+          ["x=50" #(get-trapezoid-integral-mem-var-2 f 50 h)]
+          ["x=48" #(get-trapezoid-integral-mem-var-2 f 48 h)]
+          ["x=30" #(get-trapezoid-integral-mem-var-2 f 30 h)]
+          ["x=72" #(get-trapezoid-integral-mem-var-2 f 72 h)]
+          ["x=71" #(get-trapezoid-integral-mem-var-2 f 71 h)]
+          ["x=5"  #(get-trapezoid-integral-mem-var-2 f 5 h)]])))
+
--- test/modern_development_methods/task_2_1_test.clj
+++ test/modern_development_methods/task_2_1_test.clj
@@ -2,18 +2,40 @@
   (:require [clojure.test :refer :all]
             [modern-development-methods.task-2.task-2-1 :as sut]))
 
-(deftest test-trapezoid-quadratic
+(def h 0.1)
+(def eps 1e-9)
+
+(defmacro time->err [expr]
+  `(binding [*out* *err*]
+     (time ~expr)))
+
+(deftest test-trapezoid-double
   (let [f (fn [x] (* x x))
+        base (sut/get-trapezoid-integral f 2 h)
+        v1   (sut/get-trapezoid-integral-mem-var-1 f 2 h)
+        v2   (sut/get-trapezoid-integral-mem-var-2 f 2 h)
         exact (/ 8.0 3.0)]
-    (is (<= (Math/abs (double (- exact (sut/get-trapezoid-integral f 2.0)))) (* exact 0.05)))))
+    (is (< (Math/abs (double (- base exact))) (* 0.05 exact)))
+    (is (< (Math/abs (double (- base v1))) eps))
+    (is (< (Math/abs (double (- base v2))) eps))))
 
 
 (deftest test-trapezoid-double
   (let [f (fn [x] (* x 2))
+        base (sut/get-trapezoid-integral f 5 h)
+        v1   (sut/get-trapezoid-integral-mem-var-1 f 5 h)
+        v2   (sut/get-trapezoid-integral-mem-var-2 f 5 h)
         exact 25.0]
-    (is (= exact (sut/get-trapezoid-integral f 5)))))
+    (is (< (Math/abs (double (- base exact))) (* 0.05 exact)))
+    (is (< (Math/abs (double (- base v1))) eps))
+    (is (< (Math/abs (double (- base v2))) eps))))
 
 (deftest test-trapezoid-quadratic
   (let [f (fn [x] (+ (* x x) (* 5 x) 2))
+        base (sut/get-trapezoid-integral f 30 h)
+        v1   (sut/get-trapezoid-integral-mem-var-1 f 30 h)
+        v2   (sut/get-trapezoid-integral-mem-var-2 f 30 h)
         exact 11310.0]
-    (is (<= (Math/abs (double (- exact (sut/get-trapezoid-integral f 30)))) (* exact 0.05)))))
+    (is (< (Math/abs (double (- base exact))) (* 0.05 exact)))
+    (is (< (Math/abs (double (- base v1))) eps))
+    (is (< (Math/abs (double (- base v2))) eps))))
