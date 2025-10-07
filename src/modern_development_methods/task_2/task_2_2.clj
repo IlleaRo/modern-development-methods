1e3fb139434f35e692dc9d333b565cf08fcbaf3c
IlleaRo <aro.kharin@gmail.com>
2025-10-07 20:20:06 +0700

    2.2: Добавил реализацию через lazy-cat и отдельный бенчмарк для seq

--- src/modern_development_methods/task_2/task_2_2.clj
+++ src/modern_development_methods/task_2/task_2_2.clj
@@ -15,25 +15,22 @@
 
 
 (defn trapezoid-integral-stream [f h]
-  (let [integral-step (fn [[x prev-sum]]                    ; на вход ф-ии предыдущая точка и значение в ней
-                        (let [next-x (+ x h)                ; next-x - следующая точка для вычисления
-                              next-sum (+ prev-sum          ; Вычисляем значение суммы в новой точке
-                                          (* h (/ (+ (f x) (f next-x)) 2.0)))] ; Площадь трапеции
-                          [next-x next-sum]))]
+  (let [step (fn [[x prev-sum]]                             ; на вход ф-ии предыдущая точка и значение в ней
+               (let [next-x (+ x h)                         ; next-x - следующая точка для вычисления
+                     next-sum (+ prev-sum                   ; Вычисляем значение суммы в новой точке
+                                 (* h (/ (+ (f x) (f next-x)) 2.0)))] ; Площадь трапеции
+                 [next-x next-sum]))]
     (->> [0.0 0.0]
-         (iterate integral-step)
+         (iterate step)
          (map (fn [[x sum]] [x sum])))))                    ; сохраняем пары [координата, значение]
 
 ; var 1. Только для определенной ф-ии и шага
 ; ----------------------------------------------
-(def specific-integral-seq (trapezoid-integral-stream test_f test_h))
-
-(defn get-integral-at-specific [x]
-  (let [steps (long (/ x test_h))]
+(defn get-silly-trapezoid-integral [f x h]
+  (let [steps (long (quot x test_h))
+        specific-integral-seq (trapezoid-integral-stream f h)]
     (second (nth specific-integral-seq steps))))
-
 ; ----------------------------------------------
-
 
 
 ; var 2. Для разных ф-ий и шага, но с мемоизацией
@@ -41,24 +38,52 @@
 
 (def memoized-integral-stream (memoize trapezoid-integral-stream)) ; Мемоизация по ф-ям и шагам
 
-(defn get-trapezoid-stream-integral [f x h]
+(defn get-mem-trapezoid-stream-integral [f x h]             ; Используется только для функциональных тестов
   (let [integral-seq (memoized-integral-stream f h)         ; Нужно работать с заданным h
-        steps (long (/ x h))]
+        steps (long (quot x h))]
     (second (nth integral-seq steps))))
 ; ----------------------------------------------
 
+; var 3. С помощью lazy-cat
+; ----------------------------------------------
 
-(defn measure-ms [f]
-  (let [t0 (System/nanoTime)
-        _ (f)
-        t1 (System/nanoTime)]
-    (/ (- t1 t0) 1e6)))                                     ; миллисекунды
+(defn trapezoid-integral-lazy [f h]
+  (letfn [(step [[x prev-sum]]
+            (let [next-x (+ x h)
+                  next-sum (+ prev-sum (* h 0.5 (+ (f x) (f next-x))))]
+              [next-x next-sum]))
+          (go [state]
+            (lazy-cat [state] (go (step state))))]          ; cons текущего узла и ленивый хвост
+    (go [0.0 0.0])
+    )
+  )
+; ----------------------------------------------
 
-(defn do-test [f n]
+(defn get-lazy-cat-trapezoid-integral [f x h]               ; Используется только для функциональных тестов
+  (let [steps (long (quot x test_h))
+        specific-integral-seq (trapezoid-integral-lazy f h)]
+    (second (nth specific-integral-seq steps))))
+
+; ----------------------------------------------
+
+(defn do-test-func [f n]
   (let [times (for [i (range n)]
-                (let [m (+ 1 i)                   ; увеличиваем m каждый шаг
+                (let [m (+ 1 i)                             ; увеличиваем m каждый шаг
                       t0 (System/nanoTime)
                       _ (f test_f m test_h)
+                      t1 (System/nanoTime)]
+                  (/ (- t1 t0) 1e6)))]
+    {:avg (/ (reduce + times) n)
+     :min (apply min times)
+     :max (apply max times)}))
+
+
+(defn do-test-seq [seq n]
+  (let [stream-seq (seq test_f test_h)
+        times (for [i (range n)]
+                (let [m (+ 1 i)                             ; увеличиваем m каждый шаг
+                      t0 (System/nanoTime)
+                      _ (second (nth stream-seq m))
                       t1 (System/nanoTime)]
                   (/ (- t1 t0) 1e6)))]
     {:avg (/ (reduce + times) n)
@@ -70,7 +95,10 @@
 (defn -main
   [& _]
   (println "Basic func:")
-  (println (do-test get-trapezoid-integral 100))
-  (println (do-test get-trapezoid-integral 1000))
-  (println "Stream func:")
-  (println (do-test get-trapezoid-stream-integral 1000)))
+  (println (do-test-func get-trapezoid-integral 100))
+  (println (do-test-func get-trapezoid-integral 1000))
+  (println "Streams:")
+  (println (do-test-seq trapezoid-integral-stream 1000))
+  (println (do-test-seq trapezoid-integral-lazy 1000))
+  (println "Stream with memomize func:")
+  (println (do-test-func get-mem-trapezoid-stream-integral 1000)))
--- test/modern_development_methods/task_2/task_2_2_test.clj
+++ test/modern_development_methods/task_2/task_2_2_test.clj
@@ -8,24 +8,36 @@
 (deftest test-trapezoid-double
   (let [f (fn [x] (* x x))
         base (sut/get-trapezoid-integral f 2 h)
-        stream   (sut/get-trapezoid-stream-integral f 2 h)
+        stream-mem   (sut/get-mem-trapezoid-stream-integral f 2 h)
+        stream-lazy-1  (sut/get-silly-trapezoid-integral f 2 h)
+        stream-lazy-2  (sut/get-lazy-cat-trapezoid-integral f 2 h)
         exact (/ 8.0 3.0)]
     (is (< (Math/abs (double (- base exact))) (* 0.05 exact)))
-    (is (< (Math/abs (double (- base stream))) eps))))
+    (is (< (Math/abs (double (- base stream-mem))) eps))
+    (is (< (Math/abs (double (- base stream-lazy-1))) eps))
+    (is (< (Math/abs (double (- base stream-lazy-2))) eps))))
 
 
 (deftest test-trapezoid-double
   (let [f (fn [x] (* x 2))
         base (sut/get-trapezoid-integral f 5 h)
-        stream   (sut/get-trapezoid-stream-integral f 5 h)
+        stream   (sut/get-mem-trapezoid-stream-integral f 5 h)
+        stream-lazy-1  (sut/get-silly-trapezoid-integral f 5 h)
+        stream-lazy-2  (sut/get-lazy-cat-trapezoid-integral f 5 h)
         exact 25.0]
     (is (< (Math/abs (double (- base exact))) (* 0.05 exact)))
-    (is (< (Math/abs (double (- base stream))) eps))))
+    (is (< (Math/abs (double (- base stream))) eps))
+    (is (< (Math/abs (double (- base stream-lazy-1))) eps))
+    (is (< (Math/abs (double (- base stream-lazy-2))) eps))))
 
 (deftest test-trapezoid-quadratic
   (let [f (fn [x] (+ (* x x) (* 5 x) 2))
         base (sut/get-trapezoid-integral f 30 h)
-        stream   (sut/get-trapezoid-stream-integral f 30 h)
+        stream   (sut/get-mem-trapezoid-stream-integral f 30 h)
+        stream-lazy-1  (sut/get-silly-trapezoid-integral f 30 h)
+        stream-lazy-2  (sut/get-lazy-cat-trapezoid-integral f 30 h)
         exact 11310.0]
     (is (< (Math/abs (double (- base exact))) (* 0.05 exact)))
-    (is (< (Math/abs (double (- base stream))) eps))))
+    (is (< (Math/abs (double (- base stream))) eps))
+    (is (< (Math/abs (double (- base stream-lazy-1))) eps))
+    (is (< (Math/abs (double (- base stream-lazy-2))) eps))))
